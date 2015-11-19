3df1ebb69c57032013565c012058ba6ced85f078
Jacqmin Sébastien <seb@tellthem.be>
2015-11-19 14:10:29 +0100

    fix double button

--- controller.php
+++ controller.php
@@ -205,10 +205,11 @@ class Controller extends \Concrete\Core\Package\Package {
 									$colors = PresetColors::GetColorsFromPage();
 									$session->set('anitya.colors',$colors);
 							endif;
-
-							if (!is_object($c)) return;
+							if (!is_object($c) || $GLOBALS['anityaButtonInitialized']) return;
+							$GLOBALS['anityaButtonInitialized'] = true;
 							// Now we build the button
 							$pt = $c->getCollectionThemeObject();
+
 							if ($pt->getThemeHandle() != 'anitya') return;
 							$status = t('Anitya Options');
 							$icon = 'toggle-on';
