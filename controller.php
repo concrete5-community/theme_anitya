ac4290bf720591d7f29acbd1e404db811223f425
Jacqmin Sébastien <seb@tellthem.be>
2015-11-27 14:56:47 +0100

    version update

--- controller.php
+++ controller.php
@@ -38,7 +38,7 @@ class Controller extends \Concrete\Core\Package\Package {
 	protected $pkgHandle = 'theme_anitya';
 	protected $themeHandle = 'anitya';
 	protected $appVersionRequired = '5.7.3';
-	protected $pkgVersion = '1.2.5';
+	protected $pkgVersion = '2.0';
 	protected $pkg;
 	protected $pkgAllowsFullContentSwap = true;
 	protected $startingPoint;
